cbdd10bf19c0a46350c04853e4b45a5cf8dbe04f
Hazal <hazal.helin.1907@hotmail.com>
2024-04-30 13:51:03 +0300

    commit

--- hastaneyonetimsistemi/SqlConn.cs
+++ hastaneyonetimsistemi/SqlConn.cs
@@ -10,7 +10,7 @@ namespace hastaneyonetimsistemi
     {
         public SqlConnection baglanti()
         {
-            SqlConnection con = new SqlConnection(@"Data source=DESKTOP-NH3FRG4;Initial Catalog=hastaneproje;Integrated Security=True");
+            SqlConnection con = new SqlConnection(@"Data source=HAZAL;Initial Catalog=hastaneproje;Integrated Security=True");
             con.Open();
             return con;
         }
